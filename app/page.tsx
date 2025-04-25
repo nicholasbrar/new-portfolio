20d018f627bf73247015df9799aa077aca91ec3b
Nicholas Brar <153023837+nicholasbrar@users.noreply.github.com>
2025-04-25 01:01:29 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,12 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         My Portfolio
       </h1>
+
+      <p className="mb-8">
+        Hi! I'm a developer with a keen interest in software development and AI. I enjoy working on projects that help improve efficiency and make everyday tasks easier for users. 
+        Currently, I'm at Dealer Shock building an AI-powered solutions for car dealerships to improve customer interactions and support sales. Outside of coding, I enjoy playing Geoguessr and chess competitively.
+      </p>
+      
       <p className="mb-4">
         {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
         Vim's keystroke commands and tabs' flexibility for personal viewing
